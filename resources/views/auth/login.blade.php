51d1799112f6e7dfd5155b5d2ad4420bd43cd1f6
RianKNG <belajartulus2021@gmail.com>
2023-04-12 01:54:28 +0700

    menambahkan pencarian tanngal33

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,25 +2,25 @@
 <html>
 <head>
 	<title>Login E-DIL</title>
-	{{-- <link rel="stylesheet" type="text/css" href="css/style.css"> --}}
+	<link rel="stylesheet" type="text/css" href="{{ asset('adminLTE') }}/css/style.css">
 	<link rel="stylesheet" href="{{ asset('adminLTE') }}/dist/css/style.css">
 	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
 	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-	{{-- <img class="wave" src="img/wave.png"> --}}
+	<img class="wave" src="{{ asset('adminLTE/dist/img/wave.png') }}">
 	<div class="container">
 		<div class="img">
 			{{-- <img src="img/bg.svg"> --}}
-			<img src="{{ asset('adminLTE/dist/img/a.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+			<img src="{{ asset('adminLTE/dist/img/a.png') }}" width="500px" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
 		</div>
 		<div class="login-content">
             <form method="POST" action="{{ route('login') }}">
                 @csrf
 				<img src="{{ asset('adminLTE/dist/img/avatar.svg') }}">
 				<h2 class="title"></h2>
-                <h1><span>Selamat Datang di E-DIL</span></h1>
+                <h1><span>Selamat Datang di E-Sif-DIL</span></h1>
 				
            		<div class="input-div one">
            		   <div class="i">
