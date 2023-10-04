c484831c9be5322b16fb246a3cf4afa4387a404f
RianKNG <belajartulus2021@gmail.com>
2023-10-05 06:47:35 +0700

    tambah tampilan2

--- resources/views/templates/v_sidebar.blade.php
+++ resources/views/templates/v_sidebar.blade.php
@@ -9,29 +9,31 @@
            with font-awesome or any other icon font library -->
            <li class="{{ Request::is('/')? "active":"" }}">
         <a href="/" class="nav-link">
-            <i class="nav-icon far fa-circle text-info"></i>
+          <span class="icon"><i class="fa fa-signal text-warning"></i></span>
             <p class="btn-xs">Dashboard</p>
           </a>
       </li>
       <li class="{{ Request::is('/test')? "active":"" }}">
         <a href="/test" class="nav-link">
-            <i class="nav-icon far fa-circle text-info"></i>
+          <span class="icon"><i class="fa fa-coins text-red"></i></span>
             <p class="btn-xs">Dashboard Accesories</p>
           </a>
       </li>
     <li>
-        <label for="">Data Dil</label>
+      <i><label for="">Data Dil</label></i>
+        
     </li>
     <li class="{{ Request::is('/dil')? "active":"" }}">
       <a href="/dil" class="nav-link">
-        <i class="nav-icon far fa-circle text-danger"></i>
+        <span class="icon"><i class="fa fa-home text-primary"></i></span>
         <p class="btn-xs">
           Master DIL  
         </p>
       </a>
     </li>
     <li>
-      <label for="">Layanan</label>
+      <i><label for="">Layanan</label></i>
+      
     </li>
     <li class="{{ Request::is('/dil/add')? "active":"" }}">
       <a href="/dil/add" class="nav-link">
@@ -44,7 +46,7 @@
     <li class="{{ Request::is('/penutupan')? "active":"" }}">
       <a href="/penutupan" class="nav-link">
         {{-- <i class="nav-icon far fa-circle text-blue"></i> --}}
-        <i class="nav-icon far fa-circle text-danger"></i>
+        <span class="icon"><i class="fa fa-share text-primary"></i></i></span>
         <p class="btn-xs">
           Penutupan
         </p>
@@ -61,7 +63,7 @@
     <li class="{{ Request::is('/penyambungan')? "active":"" }}">
       <a href="/penyambungan" class="nav-link">
         {{-- <i class="nav-icon far fa-circle text-success"></i> --}}
-        <i class="nav-icon far fa-circle text-success"></i>
+        <span class="icon"><i class="fa fa-arrow-down text-success"></i></span>
         <p class="btn-xs">
           Penyambungan
         </p>
@@ -70,7 +72,7 @@
     <li class="{{ Request::is('/penggantian')? "active":"" }}">
       <a href="/penggantian" class="nav-link">
         {{-- <i class="nav-icon far fa-circle text-white"></i> --}}
-        <i class="nav-icon far fa-circle text-warning"></i>
+        <span class="icon"><i class="fa fa-arrow-up text-warning"></i></span>
         <p class="btn-xs">
           Penggantian
         </p>
@@ -79,63 +81,70 @@
     <li class="{{ Request::is('/bbn')? "active":"" }}">
       <a href="/bbn" class="nav-link">
         {{-- <i class="nav-icon far fa-circle text-green"></i> --}}
-        <i class="nav-icon far fa-circle text-primary"></i>
+        <span class="icon"><i class="fa fa-file text-info"></i></span>
         <p class="btn-xs">
           Balik Nama
         </p>
       </a>
     </li>
     <li>
-      <label for="">Master Data</label>
+      <i><label for="">Master ACC</label></i>
+      
     </li>
     <li class="{{ Request::is('/watermeter')? "active":"" }}">
       <a href="/watermeter" class="nav-link">
-        <i class="nav-icon far fa-circle text-warning"></i>
+        <span class="icon"><i class="fa fa-coins text-red"></i></span>
+     
         <p class="btn-xs">
-          Master WM
+          <i>Water Meter</i>
         </p>
       </a>
     </li>
     <li class="{{ Request::is('/golongan')? "active":"" }}">
       <a href="/golongan" class="nav-link">
-        <i class="nav-icon far fa-circle text-warning"></i>
+        <span class="icon"><i class="fa fa-code-branch text-primary"></i></i></span>
         <p class="btn-xs">
-          Master Golongan
+          Golongan
         </p>
       </a>
     </li>
     @if (auth()->user()->level =="admin")
     <li>
-      <label for="">Managenent User</label>
+      <i> <label for="">Managenent User</label></i>
+     
     </li>
       <li class="{{ Request::is('/user')? "active":"" }}">
         <a href="/user" class="nav-link">
-          <i class="nav-icon far fa-circle text-danger"></i>
+          <span class="icon"><i class="fa fa-user"></i></span>
           <p class="btn-xs">
-            Master User
+            <i>Master User</i>
+            
           </p>
         </a>
       </li>
      
      @endif
       <li>
-        <label for="">Laporan</label>
+        <i> <label for="">Laporan</label></i>
+       
       </li>
 
       <li class="{{ Request::is('/layanan')? "active":"" }}">
       {{-- <li class="nav-item"> --}}
         <a href="/layanan" class="nav-link">
-          <i class="nav-icon fas fa-book"></i>
+          <i class="nav-icon fas fa-book text-warning"></i>
           <p class="btn-xs">
-            Layanan
+            <i>Layanan</i>
+            
           </p>
         </a>
       </li>
       <li class="{{ Request::is('/layanan')? "active":"" }}">
         <a href="/report" class="nav-link">
-          <i class="nav-icon fas fa-user"></i>
+          <span class="icon"><i class="fa fa-pen text-success"></i></span>
           <p class="btn-xs">
-            Report Pelanggan
+            <i>Report Pelanggan</i>
+            
           </p>
         </a>
       </li>
